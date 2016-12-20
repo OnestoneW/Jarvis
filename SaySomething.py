d2a6d8ce81fc3a2bdcd8a82bddc8ce1d1658e7e9
root <root@localhost.localdomain>
2016-12-20 21:56:16 +0100

    added the say something function

---
+++ SaySomething.py
@@ -0,0 +1,12 @@
+import subprocess
+from Joke import getRandomJoke
+
+def say_something(text):
+
+    command = "say " + text
+    process = subprocess.Popen(command.split(), stdout=subprocess.PIPE)
+    output, error = process.communicate()
+
+if __name__ == "__main__":
+    joke = getRandomJoke()
+    say_something(joke)
